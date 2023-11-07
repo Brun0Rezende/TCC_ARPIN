31dbdddd18f382e8621cb5994d22443c051721d4
gustavodscruz <gustavodiasdsc@gmail.com>
2023-11-07 15:20:08 -0300

    import login

--- lib/user_profile.dart
+++ lib/user_profile.dart
@@ -1,4 +1,4 @@
-import 'package:ar_pin/login.dart';
+import 'package:ar_pin/auth/Login.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
